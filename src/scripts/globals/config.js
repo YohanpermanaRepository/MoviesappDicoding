a8f2d3d3a699294ed8455a748edeff3835b34d27
YOHAN PERMANA <105445396+YohanpermanaRepository@users.noreply.github.com>
2024-05-08 15:42:12 +0700

    Update config.js

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  KEY: 'YOUR_API_KEY',
+  KEY: 'a45f7e420c1ce4130b7e1652dab51ff1',
   BASE_URL: 'https://api.themoviedb.org/3/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
   DEFAULT_LANGUAGE: 'en-us',
